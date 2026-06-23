1fb2f2faeb2443ba244bde13f342ad427d812b51
Vicente Bravo <vic.bravoe@duocuc.cl>
2026-06-23 15:11:47 -0400

    Update SwaggerConfig.java

--- categorias/categorias/src/main/java/com/categorias/categorias/swagger/SwaggerConfig.java
+++ categorias/categorias/src/main/java/com/categorias/categorias/swagger/SwaggerConfig.java
@@ -1,3 +1,20 @@
+package com.categorias.categorias.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Click & Eat — ms-categorias")
+                        .description("API REST para la gestión de las categorias de la aplicación Click & Eat")
+                        .version("1.0.0")
+                );
+    }
 
 }
