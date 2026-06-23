6a45ffeb0cd72f6dc57ca23b353e4c39b6931725
Vicente Bravo <vic.bravoe@duocuc.cl>
2026-06-23 15:06:50 -0400

    Update SwaggerConfig.java

--- carrito/carrito/carrito/src/main/java/com/carrito/carrito/swagger/SwaggerConfig.java
+++ carrito/carrito/carrito/src/main/java/com/carrito/carrito/swagger/SwaggerConfig.java
@@ -1,3 +1,20 @@
+package com.carrito.carrito.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Click & Eat — ms-carrito")
+                        .description("API REST para la gestión de los carritos de compra de la aplicación Click & Eat")
+                        .version("1.0.0")
+                );
+    }
 
 }
