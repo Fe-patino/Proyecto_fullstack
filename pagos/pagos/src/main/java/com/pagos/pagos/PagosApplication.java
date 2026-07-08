f36130de2543508a97516d161033aead039ae98b
DylanDuoc <dyla.riquelme@duocuc.cl>
2026-07-07 21:14:05 -0400

    Corrijo PagosAplication.java con @LoadBalanced

--- pagos/pagos/src/main/java/com/pagos/pagos/PagosApplication.java
+++ pagos/pagos/src/main/java/com/pagos/pagos/PagosApplication.java
@@ -2,20 +2,20 @@ package com.pagos.pagos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PagosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PagosApplication.class, args);
-	}
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+    public static void main(String[] args) {
+        SpringApplication.run(PagosApplication.class, args);
     }
 
-
-
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
