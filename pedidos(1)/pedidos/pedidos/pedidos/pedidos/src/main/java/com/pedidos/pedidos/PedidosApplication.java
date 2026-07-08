73baf9385f0b63a0c48493f1766a1604fad66e08
Felix Patino <fe.patino@duocuc.cl>
2026-07-07 23:50:23 -0400

    Agregar @LoadBalanced en PedidosApplication

--- pedidos(1)/pedidos/pedidos/pedidos/pedidos/src/main/java/com/pedidos/pedidos/PedidosApplication.java
+++ pedidos(1)/pedidos/pedidos/pedidos/pedidos/src/main/java/com/pedidos/pedidos/PedidosApplication.java
@@ -2,19 +2,20 @@ package com.pedidos.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PedidosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PedidosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosApplication.class, args);
+    }
 
-	@Bean
+    @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
 }
